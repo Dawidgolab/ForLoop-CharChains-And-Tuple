c31a771b9eecdcb66126554bd85d67dc8ea3af59
Dawidgolab <dawidg122@interia.pl>
2023-12-13 14:12:45 +0100

    start and end index

---
+++ Tasks/Cut_the_pizza.py
@@ -0,0 +1,24 @@
+def start_end():
+    start = int(input("Give the start index: "))
+    end = int(input("Give the end index: "))
+
+
+
+
+word = "pizza"
+
+print("""
+                  Pattern
+
+index ->    0   1   2   3   4   5
+            +---+---+---+---+---+
+            | p | i | z | z | a |
+            +---+---+---+---+---+
+index ->   -5  -4  -3  -2  -1    
+
+""")
+
+
+
+print("Provide a start and end index to cut the pizza: ")
+start_end()
