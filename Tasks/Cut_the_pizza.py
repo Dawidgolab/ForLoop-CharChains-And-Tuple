39ed39eb6cfacd83263416988817233a930a4fc1
Dawidgolab <dawidg122@interia.pl>
2023-12-13 14:48:25 +0100

    Solved the problem <function new_word at 0x000001FA834794E0>

--- Tasks/Cut_the_pizza.py
+++ Tasks/Cut_the_pizza.py
@@ -1,17 +1,21 @@
 def start_end_index():
+    print("Provide a start and end index to cut the pizza: ")
     start = int(input("Give the start index: "))
     end = int(input("Give the end index: "))
     return start,end
 
 def new_word(word):
-    start,end = start_end_index()
-    newWord = " "
-    for index in range(start,end+1):
+    start, end = start_end_index()
+    newWord = ""
+    for index in range(start, end + 1):
         letter = word[index]
         newWord += letter
-    print(f"word: [ {start} : {end} ]  -> {new_word}",end="")
+    return f"word: [ {start} : {end} ]  -> {newWord}"
 
 
+
+
+# Main
 
 word = "pizza"
 print("""
@@ -25,8 +29,4 @@ index ->   -5  -4  -3  -2  -1
 
 """)
 
-
-
-print("Provide a start and end index to cut the pizza: ")
-result = new_word(word)
-print(result)
+print(new_word(word))
