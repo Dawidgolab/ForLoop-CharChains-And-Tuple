046f3d4f18b5c999d426ab4e12bbfe674b0762f7
Dawidgolab <dawidg122@interia.pl>
2023-12-13 14:31:57 +0100

    Secound function with new word(index-letter)

--- Tasks/Cut_the_pizza.py
+++ Tasks/Cut_the_pizza.py
@@ -1,12 +1,19 @@
-def start_end():
+def start_end_index():
     start = int(input("Give the start index: "))
     end = int(input("Give the end index: "))
+    return start,end
 
+def new_word(word):
+    start,end = start_end_index()
+    newWord = " "
+    for index in range(start,end+1):
+        letter = word[index]
+        newWord += letter
+    print(f"word: [ {start} : {end} ]  -> {new_word}",end="")
 
 
 
 word = "pizza"
-
 print("""
                   Pattern
 
@@ -21,4 +28,5 @@ index ->   -5  -4  -3  -2  -1
 
 
 print("Provide a start and end index to cut the pizza: ")
-start_end()
+result = new_word(word)
+print(result)
